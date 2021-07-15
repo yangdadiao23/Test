b727b36afb84a30a4e2b573a142ed7406211234a
Yc <1549836766@qq.com>
2021-07-15 17:59:14 +0800

    第一次测试

---
+++ src/Demo.java
@@ -0,0 +1,12 @@
+/**
+ * @class
+ * @descirption
+ * @auther 杨晨
+ * @number 1910400520
+ * @date 2021-07-15
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("idea->github");
+    }
+}
